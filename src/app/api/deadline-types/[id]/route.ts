7d8fc12ccd06452c9f94231f5815dd4b40949a3e
dannysilverdev <dannysilvercross@gmail.com>
2025-07-11 23:04:12 -0400

    modifica ignorar errores de slint para build 2

--- src/app/api/deadline-types/[id]/route.ts
+++ src/app/api/deadline-types/[id]/route.ts
@@ -1,17 +1,15 @@
 import { supabaseAdmin } from '@/lib/supabaseAdmin'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params
   const body = await req.json()
+
+  const { name, measure_by, unit } = body
 
   const { error } = await supabaseAdmin
     .from('deadline_types')
-    .update({
-      name: body.name,
-      measure_by: body.measure_by,
-      unit: body.unit
-    })
+    .update({ name, measure_by, unit })
     .eq('id', id)
 
   if (error) {
